3547526280b4c766995981212b448a54ea621623
skubidanki@gmail.com <example@gmail.com>
2024-05-06 10:53:08 +0200

    Rozwinięcie programu, zmiana menu, stworzenie podstawowego kreatora miesiąca

--- KreatorMiesiaca/kreatorMiesiaca.py
+++ KreatorMiesiaca/kreatorMiesiaca.py
@@ -1,8 +1,42 @@
+import json
 
+okresRozliczeniowy = []
 def kreatorMiesiaca():
-    print("Witam w kreatorze wydatkow")
 
-    '''
-    tu będzie logika biznesowa dla kreatora wydatków za dany miesiąc
-    1. wybór miesiąca
-    '''
+    print("Witamy w kreatorze wydatków")
+    wyborRokRozliczeniowy = input("Krok 1: Wprowadź rok rozliczeniowy: ")
+    wyborMiesiacaRozliczenia = input("Krok 2: Wybierz miesiąc rozliczenia: ")
+    okresRozliczeniowy.append((wyborRokRozliczeniowy, wyborMiesiacaRozliczenia))
+
+    while True:
+        wpiszWydatek = input("Krok 3: Wprowadź nazwę wydatku: ")
+        wpiszKwoteWydatku = input("Wprowadź kwotę wydatku: ")
+        okresRozliczeniowy.append((wpiszWydatek, wpiszKwoteWydatku))
+
+        czyChceszKontynowac = input("Czy chcesz kontynuować dodawanie wydatków? TAK / NIE :")
+        while not (czyChceszKontynowac.upper() == "TAK" or czyChceszKontynowac.upper() == "NIE"):
+            print("Wybrałeś złą opcję")
+            czyChceszKontynowac = input("Czy chcesz kontynuować dodawanie wydatków? TAK / NIE :")
+
+        if czyChceszKontynowac.upper() == "TAK":
+            print("Kontynuacja...")
+        elif czyChceszKontynowac.upper() == "NIE":
+            print("Koniec dodawania wydatków.")
+            break
+
+    nazwaPlikuZapis = wyborMiesiacaRozliczenia + wyborRokRozliczeniowy
+    sciezkaZapisuPliku = "ZapisanePliki/" + nazwaPlikuZapis + ".json"
+
+    with open(sciezkaZapisuPliku, "w", encoding="utf-8") as plik:
+            json.dump(okresRozliczeniowy, plik, ensure_ascii=False)
+
+
+
+
+
+
+
+
+
+
+
--- menu.py
+++ menu.py
@@ -1,29 +1,20 @@
-import datetime
+import json
 from Kredyty import kredyty
 from KreatorMiesiaca import kreatorMiesiaca
 
 def menu():
     while True:
         print("\nMenu główne")
-        print("1. Kreator miesiąca", "2. Kredyty", "3. Pożyczki", "4. Raty", "5. Inne wydatki", "6. Koniec programu", sep="\n" )
+        print("1. Kreator miesiąca", "2. Podgląd wydatków", "3. Koniec programu" , sep="\n" )
 
         wybor = int(input("Jakiego wyboru chcesz dokonać?: "))
         if wybor == 1:
             kreatorMiesiaca.kreatorMiesiaca()
         elif wybor == 2:
-            kredyty.kredyty()
+            with open("ZapisanePliki/Styczeń2024.json", "r", encoding="utf-8") as plik:
+                dane_z_pliku = json.load(plik)
+            print(dane_z_pliku)
         elif wybor == 3:
-            print("Będzie info o ratach")
-        elif wybor == 4:
-            import datetime
-
-            x = datetime.datetime(2024, 5, 5)
-
-            print(x.strftime("%A"))
-
-        elif wybor == 5:
-            pass
-        elif wybor == 6:
             print("Dziękuje za skorzystanie z programu")
             break
         else:
